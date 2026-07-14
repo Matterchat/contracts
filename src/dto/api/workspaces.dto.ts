0bdee2b8fe278788f878a8baf99bd7c33e140cb7
Martin Petr <martinpetrnp@gmail.com>
2026-07-14 19:30:17 +0200

    Add workspace dtos

--- src/dto/api/workspaces.dto.ts
+++ src/dto/api/workspaces.dto.ts
@@ -3,9 +3,19 @@ import { IsString, Length, Matches } from "class-validator";
 export class CreateWorkspaceBodyDto {
   @IsString()
   @Length(3, 64)
-  @Matches(/^[a-zA-Z0-9_-]+$/, {
-    message:
-      "Workspace name can only contain letters, numbers, underscores and dashes",
+  @Matches(/^[\x00-\x7F]*$/, {
+    message: "Workspace name must contain only ASCII characters",
   })
   name!: string;
+}
+
+export class WorkspaceModelDto {
+  id!: string;
+  name!: string;
+  createdAt!: Date;
+  updatedAt!: Date;
+
+  constructor(partial: Partial<WorkspaceModelDto>) {
+    Object.assign(this, partial);
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 export * from "./api/version.js";
 export * from "./dto/api/users.dto.js";
 export * from "./dto/api/workspaces.dto.js";
-
-
